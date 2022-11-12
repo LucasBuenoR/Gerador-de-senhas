87331016220340fd1e5d3cb2b3f494de98e4c7ae
unknown <lucaslucasbueno2015@outlook.com>
2022-11-11 22:35:34 -0300

    Melhorias do Gerador. Versão 2.0

--- Gerador.py
+++ Gerador.py
@@ -1,21 +1,29 @@
 import random
 
-print('\n[=-=-=-=-=[Gerador de senhas]=-=-=-=-=]\n')
+while True:
 
+    print('\n[=-=-=-=-=[Gerador de senhas]=-=-=-=-=]\n')
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*()_-=+çÇ[]().,;:'
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&çÇ'
 
-qnt_senha = int(input('Quantas senhas você quer gerar? '))
+    qnt_senha = int(input('Quantas senhas você quer gerar? '))
 
-qnt_caracteres = int(input('Quantos caracteres terá essa senha? '))
+    qnt_caracteres = int(input('Quantos caracteres terá essa senha? '))
 
+    print('\n------Senhas geradas------\n')
 
-print('\n------Senhas geradas------\n')
+    for senha in range(qnt_senha):
+        senhas = ''
+        for tamanho in range(qnt_caracteres):
+            senhas += random.choice(chars)
+        print(senhas, '\n')
+        arquivo = open('senhas.txt', 'a', encoding='utf-8')
+        linhas = [senhas]
+        for senhas in linhas:
+            arquivo.write(senhas)
+            arquivo.write('\n\n')
 
-for senha in range(qnt_senha):
-    senhas = ''
-    for tamanho in range(qnt_caracteres):
-        senhas += random.choice(chars)
-    print(senhas, '\n')
-
-    
+    op = str(input('Deseja sair do Gerador <S>im ou <N>ão: '))
+    if not op == "N" or op == "n":
+        print('\nPrograma finalizado...\n')
+        break
